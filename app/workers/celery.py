e6d670ce055fbfdacb628f543fb0c1bd6ab0ab22
Nathan Ward <wardnath.alt@gmail.com>
2018-03-02 17:58:43 -0500

    twitter api stub

--- app/workers/celery.py
+++ app/workers/celery.py
@@ -1,21 +1,28 @@
 from __future__ import absolute_import
 from celery import Celery
+from .twitter.twitter_api import TwitterAPI
 app = Celery('workers',broker='amqp://admin:mypass@rabbit:5672',backend='rpc://',include=['workers.tasks'])
+twitter_api = TwitterAPI()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(1.0, test.s('hello'), name='add every 10')
+  # Calls test('hello') every 10 seconds.
+  sender.add_periodic_task(5.0, test_twitter_api.s('test'), name='twitter api test')
 
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(3.0, test.s('world'), expires=10)
+  # Calls test('world') every 30 seconds
+  # sender.add_periodic_task(3.0, test.s('world'), expires=10)
 
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
-    )
+  # Executes every Monday morning at 7:30 a.m.
+  # sender.add_periodic_task(
+  #   crontab(hour=7, minute=30, day_of_week=1),
+  #   test.s('Happy Mondays!'),
+  # )
 
 @app.task
 def test(arg):
-    print(arg)
+  print(arg)
+
+@app.task
+def test_twitter_api(test_val):
+  # print(test_val)
+  print(twitter_api.get_followers())
---
+++ app/workers/twitter/twitter_api.py
@@ -0,0 +1,5 @@
+class TwitterAPI():
+  def __init__(self):
+    self.test_val = 'foo'
+  def get_followers(self):
+    return self.test_val
