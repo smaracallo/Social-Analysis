a7cb76d27a1d7dd66c64b96620836f63f7e5a19e
Nathan Ward <wardnath.alt@gmail.com>
2018-03-02 11:39:42 -0500

    add beat service

--- app/start_app.sh
+++ app/start_app.sh
@@ -3,7 +3,7 @@ echo "STARTING CONTAINER ENTRYPOINT"
 echo $(which python)
 echo $(which pip)
 pip install -r requirements.txt
-celery -A workers worker --concurrency=20 --loglevel=info &
+celery -A workers worker -B --concurrency=20 --loglevel=info &
 python -m workers.run_tasks
 echo "FINISHED WITH SCRIPT"
 sleep 500000000
--- app/workers/run_tasks.py
+++ app/workers/run_tasks.py
@@ -1,13 +1,22 @@
 from __future__ import absolute_import
 from .tasks import longtime_add
+from .celery import app
 import time
 import numpy as np
 
 if __name__ == '__main__':
-    for _ in range(0,10):
-        result = longtime_add.delay(1,2)
-        print('Task finished?',result.ready())
-        print('Task result:',result.result)
-        time.sleep(1)
-        print('Task finished"',result.ready())
-        print('Task result:',result.result)
+  # app.conf.beat_schedule = {
+  #   'add-every-30-seconds': {
+  #       'task': 'tasks.add',
+  #       'schedule': 30.0,
+  #       'args': (16, 16)
+  #   },
+  # }
+  print('RUNNING!!')
+    # for _ in range(0,10):
+    #     result = longtime_add.delay('https://jsonplaceholder.typicode.com/posts/1')
+    #     print('Task finished?',result.ready())
+    #     print('Task result:',result.result)
+    #     time.sleep(1)
+    #     print('Task finished"',result.ready())
+    #     print('Task result:',result.result)
